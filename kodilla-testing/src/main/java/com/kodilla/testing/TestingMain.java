96002780796697b24d787e39ce53363a3cc400fb
AleksMala <o.malaszuk@gmail.com>
2017-11-09 10:06:36 +0100

    new package - user

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,13 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestingMain {
     public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        Calculator calculator = new Calculator();
+        String result = simpleUser.getUsername();
 
-        calculator.add(8,5);
-        calculator.subtract(9,3);
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //"Test - pierwszy test jednostkowy"
+        Calculator calculator1 = new Calculator();
+
+        int calcResult1 = calculator1.add(7, 0);
+        if (calcResult1 != 0) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        //"Test - drugi test jednostkowy"
+        Calculator calculator2 = new Calculator();
+
+        int calcResult2 = calculator2.subtract(4, 7);
+        if (calcResult2 != 0) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
     }
 }
