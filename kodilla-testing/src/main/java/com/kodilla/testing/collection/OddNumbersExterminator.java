d00346f8f2bf8ed0113f25e2b2f40894d728be98
AleksMala <o.malaszuk@gmail.com>
2017-11-12 11:56:04 +0100

    Changes-submodule 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,17 +3,22 @@ import java.util.ArrayList;
 
 public class OddNumbersExterminator {
     private ArrayList<Integer> evenList = new ArrayList<>();
-    private int lengthOfArray;
+    private int arrayLength;
 
-    public OddNumbersExterminator(int lengthOfArray) {
-        this.lengthOfArray = lengthOfArray;
+    public OddNumbersExterminator(int arrayLength) {
+        this.arrayLength = arrayLength;
     }
 
-    public int getLengthOfArray() {
-        return lengthOfArray;
+    public int getArrayLength() {
+        return arrayLength;
+    }
+
+    public ArrayList<Integer> getEvenList(ArrayList<Integer> numbers) {
+        return evenList;
     }
 
     public int exterminate(ArrayList<Integer> numbers){
+        arrayLength = numbers.size();
         for(Integer num: numbers){
             if(num % 2 == 0) {
                 evenList.add(num);
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class CollectionTestSuite {
     @Before
     public void before(){
-        System.out.println("Test Case: begin");
+        System.out.println("Test Case - begin");
     }
     @After
     public void after(){
@@ -17,22 +17,23 @@ public class CollectionTestSuite {
     public void testOddNumbersExterminatorEmptyList() {
         //Given
         OddNumbersExterminator exam = new OddNumbersExterminator(0);
-        ArrayList <Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         //When
         exam.exterminate(numbers);
-        System.out.println("Testing Exterminator for empty List");
+        System.out.println("Testing Exterminator for Empty List");
         //Then
-        Assert.assertEquals(0,exam.exterminate(numbers));
+        Assert.assertEquals(0, exam.exterminate(numbers));
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList(){
         //Given
         OddNumbersExterminator exam = new OddNumbersExterminator(10);
+        ArrayList<Integer> numbers = new ArrayList<>();
         //When
-        exam.getLengthOfArray();
+        exam.getArrayLength();
         System.out.println("Testing while List contains numbers");
         //Then
-        Assert.assertEquals(10,exam.getLengthOfArray());
+        Assert.assertEquals(10,exam.getArrayLength());
     }
 }
