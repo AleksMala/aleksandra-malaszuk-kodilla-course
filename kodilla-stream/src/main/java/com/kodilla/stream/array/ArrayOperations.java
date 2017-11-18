01d38ca7c06ed0b3e334b6c7241acb0657b5335c
AleksMala <o.malaszuk@gmail.com>
2017-11-18 14:23:23 +0100

    Submodule7.5/ArrayOperations

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,11 +4,16 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
-    double getAverage(int[] numbers);
+    static double getAverage(int[] numbers){
 
-
-
-
-
-
+        int[] array1 = IntStream.range(0, numbers.length)
+                .toArray();
+        double array = IntStream.range(0, numbers.length)
+                .parallel()
+                .average()
+                .getAsDouble();
+        return array;
+    }
 }
+
+
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,9 +1,20 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ArrayOperationsTestSuite {
 
     @Test
-    public void testGetAverage()
+    public void testGetAverage() {
+        //Given
+        int[] array = new int[20];
+        //When
+        ArrayOperations.getAverage(array);
+        //Then
+        assertEquals(9.5, ArrayOperations.getAverage(array),0);
+    }
+
 }
