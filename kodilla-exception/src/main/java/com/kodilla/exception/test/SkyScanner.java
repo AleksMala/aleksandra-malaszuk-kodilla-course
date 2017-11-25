410820407da98305d45fc8516000d00f4a05bcba
AleksMala <o.malaszuk@gmail.com>
2017-11-25 21:28:54 +0100

    Submodule 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/SkyScanner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SkyScanner.java
@@ -5,24 +5,27 @@ import java.util.Map;
 
 public class SkyScanner {
 
-    public void findFlight(Flight flight) throws RouteNotFoundException{
+    public boolean findFlight(Flight flight) throws RouteNotFoundException{
 
-        Map<String, Boolean> flights = new HashMap<>();
-        flights.put("Krakow", true);
-        flights.put("Warsaw", false);
+        Map<String, Boolean> flights = new HashMap<>();     // tworzę obiekt mapy
+        flights.put("Warsaw", true);    // dodaję element do mapy obiektu flights
 
-        if (flights.get(flight.getDepartureAirport())){
-            throw new RouteNotFoundException();
+        for (Map.Entry map: flights.entrySet()) {   //wywołuję mapę
+            map.getKey();    // pobieram klucz
+
+            if (flights.get(flight.getDepartureAirport()) == null || flights.containsValue(false)) {    //podczas gdy wywołuję element sprawdzam czy jego wartość key równa się null
+                throw new RouteNotFoundException();     // jeśli tak jest to wywołuję Exception
+            }
         }
+        return  true;   // jeśli wartośc key jest różna od null to wywołuję mapę,tak (o to chodzi z tym true) ?
     }
 
     public static void main (String args[]){
 
-        Flight flight = new Flight("Krakow", "Warsaw");
         SkyScanner skyScanner = new SkyScanner();
 
         try {
-            skyScanner.findFlight(flight);
+            skyScanner.findFlight(new Flight ("Warsaw", "Warsaw"));
 
         } catch (RouteNotFoundException e) {
             System.out.println(e.routeNotFoundException());
