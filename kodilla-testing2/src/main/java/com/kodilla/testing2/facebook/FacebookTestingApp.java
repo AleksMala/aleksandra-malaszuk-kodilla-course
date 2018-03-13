d00f7136b04ac556e41395bea7776718e0762851
AleksMala <o.malaszuk@gmail.com>
2018-03-13 22:51:32 +0100

    Submodule 23.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,30 +8,21 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
-    public static final String XPATH_WAIT_FOR1 = "//select[@id=\"day\"]/option[1]";
-    public static final String XPATH_WAIT_FOR2 = "//select[2]";
-    public static final String XPATH_WAIT_FOR3 = "//select[3]";
-    public static final String XPATH_SELECT_DAY = "//div[@id=\"u_0_10\"]/div/span/span/select[@id=\"day\"]/option[1]";
-    public static final String XPATH_SELECT_MONTH = "//div[@class=\"_58mq_5dbb\"]/div/span/span/select[2]";
-    public static final String XPATH_SELECT_YEAR = "//div[@class=\"_58mq_5dbb\"]/div/span/span/select[3]";
+    public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"large_form\")]/div/div/span/span/select[@name= \"birthday_day\"]/option[1]";
+    public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"large_form\")]/div/div/span/span/select[@id = \"month\"]/option[1]";
+    public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"large_form\")]/div/div/span/span/select[@id = \"year\"]/option[1]";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
 
-        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR1)).isDisplayed()) ;
-
         WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT_DAY));
         Select selectBoard = new Select(selectCombo);
-        selectBoard.getFirstSelectedOption();
-
-        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR2)).isDisplayed()) ;
+        selectBoard.selectByIndex(1);
 
         WebElement selectCombo2 = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
         Select selectBoard2 = new Select(selectCombo2);
         selectBoard2.selectByIndex(2);
-
-        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR3)).isDisplayed()) ;
 
         WebElement selectCombo3 = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
         Select selectBoard3 = new Select(selectCombo3);
